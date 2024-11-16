54263517fb8216d45b6d40fcf97418d0e4df0c4c
mun0404 <munawwar4400@gmail.com>
2024-11-15 20:58:09 -0500

    Added TF publisher.

--- include/beginner_tutorials/publisher_member_function.hpp
+++ include/beginner_tutorials/publisher_member_function.hpp
@@ -2,11 +2,31 @@
  * @file publisher_member_function.hpp
  * @author Mohammed Munawwar (mmunawwa@umd.edu)
  * @version 0.1
- * @date 2024-11-06
+ * @date 2024-11-15
  * 
  * @copyright Copyright (c) 2024
  * 
+ * @license MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
+
 #pragma once
 
 #include <beginner_tutorials/srv/new_str.hpp>
@@ -23,6 +43,9 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/msg/transform_stamped.hpp>
 
 using namespace std::chrono_literals;
 
@@ -49,7 +72,7 @@ class BasicPublisher : public rclcpp::Node {
       publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
 
       pub_timer_ = this->create_wall_timer(
-          timer_interval, std::bind(&BasicPublisher::timer_callback, this));
+          std::chrono::milliseconds(500), std::bind(&BasicPublisher::timer_callback, this));
 
       service_ = this->create_service<beginner_tutorials::srv::NewStr>(
         "change_string", [this](
